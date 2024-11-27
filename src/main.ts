4a63a7cc672d86d6ca1ed4833dcb024703d82012
Vinicius <vinicivsdev@gmail.com>
2024-11-27 12:34:33 -0300

    add cors and fix error url

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors();
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
